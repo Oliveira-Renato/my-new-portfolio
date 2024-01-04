946a38abe964f43ad5e5426f1ca5df34d31d323b
Renato <renato.printf@gmail.com>
2024-01-04 14:36:21 -0300

    about doing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>
+          Introdução
+        </p>
+        <h2 className={`${styles.sectionHeadText}`}>Sobre</h2>
+      </motion.div>
+
+      <motion.p
+      variants={fadeIn('','', 0.1, 1)}
+      >
+
+      </motion.p>
+    </>
   )
 }
 
